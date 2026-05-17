09a716febcd34ce92fb33ff764556e5f0619d302
dannri <danielruizjuez@gmail.com>
2026-05-17 23:31:19 +0200

    Collapse group-label cells to zero width instead of display:none to keep OTROS column visible

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
  *
  * To force users to pick up a new app version: bump CACHE_VERSION.
  */
-const CACHE_VERSION = 'v4';
+const CACHE_VERSION = 'v5';
 const CACHE_NAME = 'avisor-de-partes-' + CACHE_VERSION;
 
 const PRECACHE_URLS = [
